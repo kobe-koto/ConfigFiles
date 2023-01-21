a4db50a7fdd4c45594c66933353de0e31c952829
kobe-koto <91669733+kobe-koto@users.noreply.github.com>
2023-01-22 00:23:23 +0800

    1

--- icalinguapp/addon.js
+++ icalinguapp/addon.js
@@ -1,4 +1,13 @@
 // 對原作 OICQ 的致敬
 document.getElementsByClassName("vac-textarea")[0].placeholder = "Shit driven NotOpenICQ 🤣👉";
 
-
+// console.log = function (obj) {
+//     if (obj.toString().match(/[object FileList]/i)) {
+//         document.getElementsByClassName("vac-box-footer")[0].childNodes[0].childNodes[0].onclick = function () {
+//             document.getElementsByClassName("vac-textarea")[0].style.height =
+//                 document.getElementsByClassName("vac-textarea")[0].style.minHeight
+//         }
+//     } else {
+//         console.info(obj)
+//     }
+// }
